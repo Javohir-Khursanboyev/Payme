f70137c7e744c40ec175207f79774a7e3edd4074
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-29 20:27:35 +0500

    Added Payment, UserPayment, PaymentCategory Entities

---
+++ src/Payme.Domain/Enitites/UserPayments/UserPayment.cs
@@ -0,0 +1,18 @@
+﻿using Payme.Domain.Commons;
+using Payme.Domain.Enitites.Payments;
+using Payme.Domain.Enitites.Users;
+
+namespace Payme.Domain.Enitites.UserPayments;
+
+public class UserPayment : Auditable
+{
+    public long UserId { get; set; }
+    public User user { get; set; }
+    public long CardId { get; set; }
+    public Card Card { get; set; }
+    public long PaymentId { get; set; }
+    public Payment Payment { get; set; }
+    public string AccountNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string AdditionalInformation { get; set; }
+}
