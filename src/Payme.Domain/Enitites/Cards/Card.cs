06ba08ef602d7d8ec63dd5dd68e4e7edf2b6a0c6
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-29 20:45:47 +0500

    Added Card and Transavtion Entities

---
+++ src/Payme.Domain/Enitites/Cards/Card.cs
@@ -0,0 +1,15 @@
+﻿using Payme.Domain.Commons;
+using Payme.Domain.Enitites.Users;
+
+namespace Payme.Domain.Enitites.Cards;
+
+public class Card : Auditable
+{
+    public long CustomerId { get; set; }
+    public User Customer { get; set; }
+    public Type Type { get; set; }
+    public long Number { get; set; }
+    public string ExpiryData { get; set; }
+    public string Password { get; set; }
+    public decimal Balance { get; set; }
+}
