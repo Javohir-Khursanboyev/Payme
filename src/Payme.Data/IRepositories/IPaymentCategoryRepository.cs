bbe02222dafbd09504f83cde74948f530418d4a9
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-30 11:37:58 +0500

    Added Repository

--- src/Payme.Data/IRepositories/IPaymentCategoryRepository.cs
+++ src/Payme.Data/IRepositories/IPaymentCategoryRepository.cs
@@ -1,5 +1,15 @@
-﻿namespace Payme.Data.IRepositories;
+﻿using Payme.Domain.Enitites.PaymentCategories;
+using Payme.Domain.Enitites.Payments;
+
+namespace Payme.Data.IRepositories;
 
 public interface IPaymentCategoryRepository
 {
+    Task<PaymentCategory> InsertAsync(PaymentCategory paymentCategory); 
+    Task<PaymentCategory> UpdateAsync(long id, PaymentCategory paymentCategory);
+    Task<bool> DeleteAsync (long id);
+    Task<PaymentCategory> SelectIdAsync (long id);
+    Task<IEnumerable<PaymentCategory>> SelectAllAsEnumerable ();
+    Task<IQueryable<PaymentCategory>> SelectAllAsQueryable ();
+    Task SaveAsync();
 }
---
+++ src/Payme.Data/Repositories/PaymentCategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Payme.Data.IRepositories;
+using Payme.Domain.Enitites.PaymentCategories;
+
+namespace Payme.Data.Repositories;
+
+public class PaymentCategoryRepository : IPaymentCategoryRepository
+{
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentCategory> InsertAsync(PaymentCategory paymentCategory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PaymentCategory>> SelectAllAsEnumerable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IQueryable<PaymentCategory>> SelectAllAsQueryable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentCategory> SelectIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentCategory> UpdateAsync(long id, PaymentCategory paymentCategory)
+    {
+        throw new NotImplementedException();
+    }
+}
