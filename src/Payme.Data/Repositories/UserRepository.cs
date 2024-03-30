7365e6ad5802f266b2278b4fc795400847a13a27
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-03-30 14:42:35 +0500

    Added UserRepository

--- src/Payme.Data/Repositories/UserRepository.cs
+++ src/Payme.Data/Repositories/UserRepository.cs
@@ -1,42 +1,86 @@
-﻿using Payme.Data.IRepositories;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Payme.Data.IRepositories;
 using Payme.Domain.Entities.Users;
 
 namespace Payme.Data.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    public Task<bool> DeleteAsync(User user)
+    private readonly string? connectionString;
+    public UserRepository(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        connectionString = configuration.GetConnectionString("DefaultConnection");
     }
 
-    public Task<User> InsertAsync(User user)
+    public async Task<User> InsertAsync(User user)
     {
-        throw new NotImplementedException();
+        var query = @"INSERT INTO Users (FirstName, LastName, Phone, Password, Role, CreatedAt, UpdatedAt, DeletedAt, IsDeleted) 
+                             VALUES (@FirstName, @LastName, @Phone, @Password, @Role, @CreatedAt, @UpdatedAt, @DeletedAt, @IsDeleted);";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            await connection.ExecuteAsync(query, user);
+        }
+        return user;
     }
 
-    public Task SaveAsync()
+    public async Task<User> UpdateAsync(User user)
     {
-        throw new NotImplementedException();
+        var query = @"UPDATE Users SET FirstName = @FirstName, LastName = @LastName, 
+                             Phone = @Phone, Password = @Password, Role = @Role,
+                             UpdatedAt = @UpdatedAt, IsDeleted = 1
+                             WHERE Id = @Id";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            await connection.ExecuteAsync(query, user);
+        }
+        return user;
+    }
+    public async Task<bool> DeleteAsync(User user)
+    {
+        var query = @"UPDATE Users 
+                        SET IsDeleted = 1, 
+                            DeletedAt = @DeletedAt 
+                        WHERE Id = @Id";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+             await connection.ExecuteAsync(query, user);
+        }
+        return true;
+    }
+    public async Task<IEnumerable<User>> SelectAllAsIEnumerable()
+    {
+        var query = "SELECT * FROM Users";
+        
+        using (var connection = new SqlConnection(connectionString))
+        {
+            return (await connection.QueryAsync<User>(query)).ToList();
+        }
     }
 
-    public Task<IEnumerable<User>> SelectAllAsIEnumerable()
+    public async Task<IQueryable<User>> SelectAllIQueryable()
     {
-        throw new NotImplementedException();
+        var query = "SELECT * FROM Users";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            return (await connection.QueryAsync<User>(query)).AsQueryable();
+        }
     }
 
-    public Task<IQueryable<User>> SelectAllIQueryable()
+    public async Task<User> SelectAsync(long id)
     {
-        throw new NotImplementedException();
-    }
+        var query = "SELECT * FROM Users WHERE Id = @id";
 
-    public Task<User> SelectAsync(long id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<User> UpdateAsync(User user)
-    {
-        throw new NotImplementedException();
+        using (var connection = new SqlConnection(connectionString))
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return await connection.QueryFirstOrDefaultAsync<User>(query,new { id = id });
+#pragma warning restore CS8603 // Possible null reference return.
+        }
     }
 }
