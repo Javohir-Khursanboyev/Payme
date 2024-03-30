f48e5b4d72d3d7301cda30bd7d571c0f64554e66
Komiljon <komiljonrahmatov03@gmail.com>
2024-03-30 20:32:24 +0500

    Added Mappers

--- src/Payme.Service/Mappers/MappingProfile.cs
+++ src/Payme.Service/Mappers/MappingProfile.cs
@@ -1,5 +1,14 @@
 ﻿using AutoMapper;
+using Payme.Domain.Entities.Cards;
+using Payme.Domain.Entities.PaymentCategories;
+using Payme.Domain.Entities.Payments;
+using Payme.Domain.Entities.Transactions;
+using Payme.Domain.Entities.UserPayments;
 using Payme.Domain.Entities.Users;
+using Payme.Service.DTOs.Cards;
+using Payme.Service.DTOs.PaymentCategories;
+using Payme.Service.DTOs.Payments;
+using Payme.Service.DTOs.Transactions;
 using Payme.Service.DTOs.Users;
 
 namespace Payme.Service.Mappers;
@@ -11,5 +20,25 @@ public class MappingProfile : Profile
         CreateMap<User, UserViewModel>().ReverseMap();
         CreateMap<User, UserCreationModel>().ReverseMap();
         CreateMap<User, UserUpdateModel>().ReverseMap();
+
+        CreateMap<Card, CardViewModel>().ReverseMap();
+        CreateMap<Card, CardCreationModel>().ReverseMap();
+        CreateMap<Card, CardUpdateModel>().ReverseMap();
+
+        CreateMap<Transaction, TransactionViewModel>().ReverseMap();
+        CreateMap<Transaction, TransactionCreationModel>().ReverseMap();
+        CreateMap<Transaction, TransactionUpdateModel>().ReverseMap();
+
+        CreateMap<Payment, PaymentViewModel>().ReverseMap();
+        CreateMap<Payment, PaymentCreationModel>().ReverseMap();
+        CreateMap<Payment, PaymentUpdateModel>().ReverseMap();
+
+        CreateMap<UserPayment, UserViewModel>().ReverseMap();
+        CreateMap<UserPayment, UserCreationModel>().ReverseMap();
+        CreateMap<UserPayment, UserUpdateModel>().ReverseMap();
+
+        CreateMap<PaymentCategory, PaymentCategoryViewModel>().ReverseMap();
+        CreateMap<PaymentCategory, PaymentCategoryCreationModel>().ReverseMap();
+        CreateMap<PaymentCategory, PaymentCategoryUpdateModel>().ReverseMap();
     }
 }
