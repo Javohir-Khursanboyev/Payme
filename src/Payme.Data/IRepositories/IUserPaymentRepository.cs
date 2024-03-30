b4429d5687388781c8eab2cbd467759d70298eca
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-30 23:55:49 +0500

    Added IUserPaymentRepository And UserPaymentRepository

--- src/Payme.Data/IRepositories/IUserPaymentRepository.cs
+++ src/Payme.Data/IRepositories/IUserPaymentRepository.cs
@@ -4,5 +4,9 @@ namespace Payme.Data.IRepositories;
 
 public interface IUserPaymentRepository
 {
-
+    Task<UserPayment> InsertAsync(UserPayment userPayment);
+    Task<UserPayment> SelectAsync(long id);
+    Task<UserPayment> DeleteAsync(long id);
+    Task<IEnumerable<UserPayment>> SelectAsIEnumerableAsync();
+    Task<IEnumerable<UserPayment>> SelectAsIQueryableAsync();
 }
