7292b73fe4f00b1f30afb6953f75b925830d54aa
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-31 13:03:20 +0500

    Added UserPaymentController

---
+++ src/Payme.WebApi/Controllers/UserPaymentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Payme.Service.DTOs.UserPayments;
+using Payme.Service.Services.UserPaymentServices;
+using Payme.WebApi.Models;
+
+namespace Payme.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserPaymentController : ControllerBase
+{
+    private readonly IUserPaymentService userPaymentService;
+
+    public UserPaymentController(IUserPaymentService userPaymentService)
+    {
+        this.userPaymentService = userPaymentService;
+    }
+
+    //POST: api<UserPaymentController>
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] UserPaymentCreationModel model)
+    {
+        return Ok(new Response()
+        {
+            StatusCode = 200,
+            Message = "OK",
+            Data = await userPaymentService.CreateAsync(model)
+        });
+    }
+
+    //GET: api<UserPaymentController>
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(new Response()
+        {
+            StatusCode = 200,
+            Message = "OK",
+            Data = await userPaymentService.GetAllAsync()
+        });
+    }
+
+    //GET: api<UserPaymentController>/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(long id)
+    {
+        return Ok(new Response()
+        {
+            StatusCode = 200,
+            Message = "OK",
+            Data = await userPaymentService.GetByIdAsync(id)
+        });
+    }
+}
