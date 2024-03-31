4cf93e723c84bbb7700ebd33b4ac7d01b91964f4
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-31 11:51:35 +0500

    Added IUserPaymentService

---
+++ src/Payme.Service/Services/UserPaymentServices/IUserPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using Payme.Service.DTOs.UserPayments;
+
+namespace Payme.Service.Services.UserPaymentServices;
+
+public interface IUserPaymentService
+{
+    Task<UserPaymentViewModel> CreateAsync(UserPaymentCreationModel model);
+    Task<UserPaymentViewModel> GetByIdAsync (long id);
+    Task<IEnumerable<UserPaymentViewModel>> GetAllAsync ();
+}
---
+++ src/Payme.Service/Services/UserPaymentServices/UserPaymentService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Payme.Data.IRepositories;
+using Payme.Service.DTOs.UserPayments;
+using Payme.Service.Services.CardServices;
+using Payme.Service.Services.UserServices;
+
+namespace Payme.Service.Services.UserPaymentServices;
+
+public class UserPaymentService : IUserPaymentService
+{
+    private readonly IUserPaymentRepository repository;
+    private readonly IUserService userService;
+    private readonly ICardService cardService;
+    private readonly 
+    private readonly 
+    private readonly IMapper mapper;
+
+    public UserPaymentService(IUserPaymentRepository repository, IMapper mapper)
+    {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public async Task<UserPaymentViewModel> CreateAsync(UserPaymentCreationModel model)
+    {
+        var userPayments = await repository.SelectAsIQueryableAsync();
+
+    }
+
+    public Task<IEnumerable<UserPaymentViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserPaymentViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+}
